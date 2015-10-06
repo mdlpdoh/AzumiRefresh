5b5c12b2c18d3e4997388fbcef02f4a3162b3881
Maria Mercedes Martinez <maria@dogonahorse.com>
2015-10-06 17:20:07 -0400

    Change floats into public variables separate into two different scripts Ball Roll and Ball Collision

--- Assets/Resources/Scripts/mmBallRoll.cs
+++ Assets/Resources/Scripts/mmBallRoll.cs
@@ -5,7 +5,16 @@ public class mmBallRoll : MonoBehaviour {
 	
 //	public mmGameManagerController gm;
 	private Rigidbody2D myRb;
-	private GameObject hits;
+//	private GameObject hits;
+
+	// speed at which ball starts rolling
+	public float startSpeed = 200f;
+
+	// speed that is added upon a click/tap
+	public float onTapSpeed = 300f;
+
+	// velocity clamp
+	public float clampSpeed = 10f;
 
 	// Use this for initialization
 	void Start () {
@@ -14,13 +23,13 @@ public class mmBallRoll : MonoBehaviour {
 //		gameObject.GetComponent<ParticleSystem> ().enableEmission = false;
 
 		// get the particle system child object of the ball
-		hits = GameObject.Find ("hitParticles");
+//		hits = GameObject.Find ("hitParticles");
 
 		// get the rigidbody
 		myRb = gameObject.GetComponent<Rigidbody2D> ();
 
 		// start ball rolling by adding force started at 500f but much better with a slower ball.
-		myRb.AddForce(transform.right * 200f);
+		myRb.AddForce(transform.right * startSpeed);
 
 		// get the game manager script
 //		gm = GameObject.Find("GameManager").GetComponent<mmGameManagerController>();
@@ -35,40 +44,12 @@ public class mmBallRoll : MonoBehaviour {
 	void FixedUpdate() {
 
 		// clamp the velocity or it will go througjh colliders...also notice Y is constrained in the rigidbody so it wont fly up due to physics
-		myRb.velocity = Vector2.ClampMagnitude(myRb.velocity, 10f);
+		myRb.velocity = Vector2.ClampMagnitude(myRb.velocity, clampSpeed);
 
 		if (Input.GetMouseButtonDown (0)) {
-			myRb.AddForce(transform.up * 100f);
+			myRb.AddForce(transform.up * onTapSpeed);
 		}
 	}
 
-//	void OnCollisionEnter(Collision coll){
-//		if (coll.collider.tag == "Wall") {
-//			gm.incBounces ();
-//			gm.getBounces ();
-//		} else if (coll.collider.tag == "success") {
-//			gm.getSuccess();
-//			Destroy(this.gameObject);
-//		}
-//	}
-
-	void OnCollisionEnter2D(Collision2D coll){
-		if (coll.transform.tag == "Block") {
-			print ("Yes its a block");
-			// below is to give a nice particle effect when ball hits something.
-			ContactPoint2D contact = coll.contacts[0];
-			Quaternion rot = Quaternion.FromToRotation(Vector3.right, contact.normal);
-			Vector2 pos = contact.point;
-			print(rot.eulerAngles);
-			hits.transform.position = pos;
-			hits.transform.rotation = rot;
-			hits.GetComponent<ParticleSystem> ().Emit(100);
-			//fix for making the particles into a V shape that always rotate on x axis
-			//make an empty game object and parent the hitParticles to it and use below 
-			//but above particles looks better for other applications.
-			//hits.GetComponentInChildren<ParticleSystem> ().Emit(100);
-
-		}
-	}
 
 }// end class
