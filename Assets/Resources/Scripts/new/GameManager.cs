2915b07700c6b9bc3f33dc3ee0afd8556a0e6713
Michael de la Pena <mdlp@dogonahorse.com>
2015-10-09 12:17:08 -0400

    Updated .gitignore

---
+++ Assets/Resources/Scripts/new/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using MonsterLove.StateMachine;
+
+namespace com.dogOnaHorse
+{
+	public class GameManager : StateBehaviour
+	{
+
+		private static GameManager instance = null;
+	
+		public static GameManager Instance { 
+		// return reference to private instance 
+		
+			get { 
+				return instance; 
+			} 
+		}
+
+		void Awake ()
+		{
+			// Check if existing instance of class exists in scene 35 
+			// If so, then destroy this instance
+
+			if (instance) {
+				DestroyImmediate (gameObject); 
+				return;
+			}
+			// Make this active and only instance
+			instance = this;
+			DontDestroyOnLoad (gameObject);
+		}
+
+
+	}
+}
