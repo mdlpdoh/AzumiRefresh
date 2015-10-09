b050c87397fca246dfd35d7b4eec3a62ff3d4898
Maria Mercedes Martinez <maria@dogonahorse.com>
2015-10-09 11:53:21 -0400

    add levels to chapter one and start to change ball react to click

--- Assets/Resources/Scripts/azumiBallRoll.cs
+++ Assets/Resources/Scripts/azumiBallRoll.cs
@@ -1,7 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class mmBallRoll : MonoBehaviour {
+public class azumiBallRoll : MonoBehaviour {
+
+	private GameObject theBall;
+	private GameObject theDoor;
 	
 //	public mmGameManagerController gm;
 	private Rigidbody2D myRb;
@@ -18,6 +21,10 @@ public class mmBallRoll : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+		// find the ball and door to move from door on click in fixedUpdate.
+		theBall = GameObject.Find ("Ball");
+		theDoor = GameObject.Find ("Door");
 
 		//make sure particle is off to start
 //		gameObject.GetComponent<ParticleSystem> ().enableEmission = false;
@@ -43,11 +50,27 @@ public class mmBallRoll : MonoBehaviour {
 
 	void FixedUpdate() {
 
-		// clamp the velocity or it will go througjh colliders...also notice Y is constrained in the rigidbody so it wont fly up due to physics
+		// clamp the velocity or it will go througjh colliders...also notice Y is constrained in the rigidbody so it wont fly up due to physics.
 		myRb.velocity = Vector2.ClampMagnitude(myRb.velocity, clampSpeed);
 
 		if (Input.GetMouseButtonDown (0)) {
-			myRb.AddForce(transform.up * onTapSpeed);
+				//get the rigidbody og the ball
+				Rigidbody2D getBall = theBall.GetComponent<Rigidbody2D> ();
+				//find the magnitude of the ball
+				float theMag = getBall.velocity.magnitude;
+				//0 out the velocity of the ball
+				//			getBall.velocity = new Vector2(0, 0);
+				//get the position of the door and the ball
+				Vector2 doorPos = theDoor.transform.position;
+				Vector2 ballPos = getBall.transform.position;
+				//get the Trajectory of the ball to the door
+				Vector2 ballTraj = ballPos - doorPos;
+				//normalize it
+				Vector2 ballTrajN = ballTraj.normalized;
+				//give the ball its new trajectory to the door
+				getBall.velocity = ballTrajN * theMag;
+
+//				myRb.AddForce (transform.up * onTapSpeed);
 		}
 	}
 
