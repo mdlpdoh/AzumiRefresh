a972b360330314247ce1e95ce592a5291f640cf0
Maria Mercedes Martinez <maria@dogonahorse.com>
2015-10-13 17:59:32 -0400

    continue chapter 2 levels

---
+++ Assets/Resources/Scripts/coin.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class coin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		if(col.gameObject.name == "Ball")
+		{
+			Destroy(this.gameObject);
+			print("Coin has been pocketed");
+		}
+	}
+
+}//end class
