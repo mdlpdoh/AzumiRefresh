8a6b96e0e05f513ea7f89f11e87f576f936acb5b
Maria Mercedes Martinez <maria@dogonahorse.com>
2015-11-12 16:54:54 -0500

    add namespacing to Chapter3level 10 and all of Chapter 4 and clean up, create prefabs

--- Assets/Resources/Scripts/moving walls/WallStopOnX.cs
+++ Assets/Resources/Scripts/moving walls/WallStopOnX.cs
@@ -1,36 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class WallStopOnX : MonoBehaviour {
-	
-	public float farLeft = -2.75f;
-	public float farRight = 2.8f;
 
-	// Use this for initialization
-	void Start () {
-	}
-
-	void OnMouseDrag() 
-	{
-//		this.GetComponent<Rigidbody2D> ().isKinematic = false;
-		Vector2 newPos = transform.position;
-		Vector2 mousePos = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
-		Vector2 objPos = Camera.main.ScreenToWorldPoint (mousePos);
-		// decide where to put the block.
-		if (objPos.x <= farLeft) {
-			newPos.x = farLeft;
-		} else if (objPos.x >= farRight) {
-			newPos.x = farRight;
-		} else {
-			newPos.x = objPos.x;
+namespace com.dogonahorse
+{
+	public class WallStopOnX : MonoBehaviour {
+		
+		public float farLeft = -2.75f;
+		public float farRight = 2.8f;
+		
+		// Use this for initialization
+		void Start () {
 		}
-		//send the block to that position.
-		transform.position = new Vector2 (newPos.x, transform.position.y);
-	}
+		
+		void OnMouseDrag() 
+		{
+			//		this.GetComponent<Rigidbody2D> ().isKinematic = false;
+			Vector2 newPos = transform.position;
+			Vector2 mousePos = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
+			Vector2 objPos = Camera.main.ScreenToWorldPoint (mousePos);
+			// decide where to put the block.
+			if (objPos.x <= farLeft) {
+				newPos.x = farLeft;
+			} else if (objPos.x >= farRight) {
+				newPos.x = farRight;
+			} else {
+				newPos.x = objPos.x;
+			}
+			//send the block to that position.
+			transform.position = new Vector2 (newPos.x, transform.position.y);
+		}
+		
+		
+		// Update is called once per frame
+		void Update () {
+		}
+		
+	}//end class
 
-
-	// Update is called once per frame
-	void Update () {
-	}
-
-}//end class
+}// end namespace
