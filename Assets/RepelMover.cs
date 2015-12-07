7d1faaa790ea68ab8ef5462cb21106d010605d48
Michael de la Pena <mdlp@dogonahorse.com>
2015-12-07 18:02:56 -0500

    Various graphics updates

--- Assets/RepelMover.cs
+++ Assets/RepelMover.cs
@@ -1,33 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class RepelMover : MonoBehaviour {
+public class RepelMover : MonoBehaviour
+{
 
-	// Use this for initialization
-	public float RepelStrength = 0.1f;
-	
-	// Update is called once per frame
-		public float startDistance =0f;
-	
-	       void OnTriggerEnter2D(Collider2D col)
-        {
-            if (col.gameObject.name == "Ball")
-            {
-				startDistance =  (col.transform.position- transform.position).magnitude;
-            }
-        }
+    // Use this for initialization
+    public float RepelStrength = 0.1f;
 
-	      void OnTriggerStay2D(Collider2D col)
+    // Update is called once per frame
+    public float startDistance = 0f;
+    public Rigidbody2D particleRotator;
+
+             public float rotatorVelocity = 150;
+    void Start()
+    {
+        particleRotator.angularVelocity = rotatorVelocity;
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Ball")
         {
-            if (col.gameObject.name == "Ball")
-            {
-             Vector2 repelVector = (col.transform.position-transform.position);
-			 
-			 float currentDistance = repelVector.magnitude/startDistance;
-			Vector2 repelNormalVector = repelVector.normalized;
-				
-			col.attachedRigidbody.AddForce(repelNormalVector * (RepelStrength * (currentDistance)));
-			// col.attachedRigidbody.AddForce(AttractNormalVector * (attractStrength * (currentDistance * currentDistance)));
-            }
+            startDistance = (col.transform.position - transform.position).magnitude;
         }
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Ball")
+        {
+            Vector2 repelVector = (col.transform.position - transform.position);
+
+            float currentDistance = repelVector.magnitude / startDistance;
+            Vector2 repelNormalVector = repelVector.normalized;
+
+            col.attachedRigidbody.AddForce(repelNormalVector * (RepelStrength * (currentDistance)));
+            // col.attachedRigidbody.AddForce(AttractNormalVector * (attractStrength * (currentDistance * currentDistance)));
+        }
+    }
 }
---
+++ Assets/Resources/Scripts/Shaders/RotateShader.shader
@@ -0,0 +1,55 @@
+﻿Shader "RotatingTexture"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+    SubShader
+    {
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+			// make fog work
+			#pragma multi_compile_fog
+					#include "UnityCG.cginc"
+			
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 pos : SV_POSITION;
+		
+            };
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+            float4x4 _TextureRotation;
+         
+		       	float4 _MainTex_ST;
+            sampler2D _MainTex;
+            v2f vert (float4 pos : POSITION, float2 uv : TEXCOORD0, appdata v)
+            {
+                v2f o;
+                o.pos = mul(UNITY_MATRIX_MVP, pos);
+                
+                
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex) * mul(_TextureRotation, float4(uv,0,1)).xy;
+                //o.uv = mul(_TextureRotation, float4(uv,0,1)).xy;
+          
+                return o;
+            }
+     
+            fixed4 frag (v2f i) : SV_Target
+            {
+                return tex2D(_MainTex, i.uv);
+            }
+            ENDCG
+        }
+    }
+}
