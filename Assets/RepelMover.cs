1f24f5e7bb246ad1a20977fdabd37e6d779e8b02
Michael de la Pena <mdlp@dogonahorse.com>
2015-12-08 11:51:05 -0500

    adjusted repel mover

--- Assets/RepelMover.cs
+++ Assets/RepelMover.cs
@@ -11,11 +11,45 @@ public class RepelMover : MonoBehaviour
     public float startDistance = 0f;
     public Rigidbody2D particleRotator;
 
-             public float rotatorVelocity = 150;
+    public float rotatorVelocity = 150;
+    
+    public float rotatorForce = 100;
+      public bool rotationIsPositive = true;
+      
+     public float rotationInterval = 0.5f;
+     
+     public ConstantForce2D particleRotatorForce;
+     
     void Start()
     {
-        particleRotator.angularVelocity = rotatorVelocity;
+           particleRotatorForce.torque = -rotatorForce* 0.7f;
+          InvokeRepeating("switchDirection",rotationInterval, rotationInterval);
+          //particleRotatorForce.torque = -rotatorForce/2;
+       // particleRotator.angularVelocity = rotatorVelocity;
+       
     }
+        void Update()
+    {
+        particleRotatorForce.torque = Mathf.Clamp(particleRotatorForce.torque,-rotatorForce,rotatorForce);
+        particleRotator.angularVelocity = Mathf.Clamp(particleRotatorForce.torque,-rotatorVelocity,rotatorVelocity);;
+    }
+    void switchDirection() {
+        if (rotationIsPositive){
+            particleRotatorForce.torque = rotatorForce;
+           //particleRotator.angularVelocity = 0;
+        } else {
+             // particleRotator.AddTorque(-rotatorForce);    
+              particleRotatorForce.torque = -rotatorForce;
+                 //particleRotator.angularVelocity = 0;
+        }
+        rotationIsPositive = !rotationIsPositive;
+    }
+    void Example() {
+      
+    }
+    
+    
+    
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.name == "Ball")
