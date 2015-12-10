3561b2473f98609a2bf778e9abd761adc86485c9
Maria Mercedes Martinez <maria@dogonahorse.com>
2015-12-10 12:07:02 -0500

    working on PreGame Modal

---
+++ Assets/Resources/Scripts/ApplicationFramework/PreGameModalWindow.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace com.dogonahorse
+{
+
+
+	public class PreGameModallWindow : ModalWindow
+	{
+
+		private string beginLevelMessage = "Eat as Many ants as you can before leaving the room.\nYou have @B\nswipes to free the captive\n@A!";
+
+
+		private ScoreManager scoreManager;
+
+
+
+	
+		override public void InitWindow() {
+			scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
+			int numberOfBounces = scoreManager.numberOfBounces;
+			string typeOfAnimal = scoreManager.ChapterAnimalName;;
+			string resultsMessage;
+
+			resultsMessage = ParseMessageString(beginLevelMessage,numberOfBounces,typeOfAnimal);
+
+			Text messageText = GetComponentInChildren<Text>();
+		//	print (messageText);
+			messageText.text = resultsMessage;
+
+		}
+
+
+		string ParseMessageString (string rawMessage, int numberOfBounces, string typeOfAnimal) {
+			rawMessage = rawMessage.Replace("@A", typeOfAnimal); 
+			rawMessage = rawMessage.Replace("@B", numberOfBounces.ToString()); 
+			return rawMessage ;
+		}
+		
+	}
+}
