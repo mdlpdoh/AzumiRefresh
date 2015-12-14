0ce6f742260a3a77e5d69f7c36d41c3b8d3877b0
Maria Mercedes Martinez <maria@dogonahorse.com>
2015-12-14 18:16:04 -0500

    Start to play and make TODO in code for swipe notification flasher

--- Assets/Resources/Scripts/FrameworkedGameScripts/ScoreCounter.cs
+++ Assets/Resources/Scripts/FrameworkedGameScripts/ScoreCounter.cs
@@ -8,23 +8,43 @@ namespace com.dogonahorse
 	public class ScoreCounter : MonoBehaviour
 	{
 		private Text myText;
+		private String flashBlank;
 
 		void Awake ()
 		{
 			myText = GetComponent<Text>();
+		
 		}
 		void Start ()
 		{
 			EventManager.ListenForEvent (AzumiEventType.SetBounces, OnSetBouncesEvent);
 
 		}
+
 		public void SetStartingAmount(int startingAmount){
 			myText.text = startingAmount.ToString();
 			print (startingAmount);
 		}
+
 		void OnSetBouncesEvent (AzumiEventType Event_Type, Component Sender, object Param = null)
 		{
 			myText.text = Param.ToString();
+
+
+			print (myText.text);
+			if (myText.text == "2") {
+				print ("START FLASHING!");
+				swipeNotifier ();
+
+			}
+
+		}
+
+		void swipeNotifier()
+		{
+			// TODO
+			// MAKE THE THE NUMBER OF SWIPES COUNTDOWN FLASH WHEN THERE ARE 4 SWIPES LEFT
 		}
 	}
 }
+
