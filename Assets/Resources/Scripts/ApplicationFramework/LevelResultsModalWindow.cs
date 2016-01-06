74beb15d8107afedff2f3bec112c77736ece1042
Maria Mercedes Martinez <maria@dogonahorse.com>
2016-01-06 18:25:21 -0500

    Made Timer fade in after player reaches 0 swipes. Fixed star score bugs

--- Assets/Resources/Scripts/ApplicationFramework/LevelResultsModalWindow.cs
+++ Assets/Resources/Scripts/ApplicationFramework/LevelResultsModalWindow.cs
@@ -9,11 +9,9 @@ namespace com.dogonahorse
 	public class LevelResultsModalWindow : ModalWindow
 	{
 
-		private string victoryMessage1 = "You freed the\n@A\nwith @B\nenergy left!";
-		private string victoryMessage2 = "You also got\n@C Ants!";
-		private string failureMessage1 = "The ants got you!";
-		private string failureMessage2 = "You didn't free the\n@A!\nBut you got\n @C coins!";
-		private string failureMessage3 = "Aargh!\nOut of energy!\nThe animals need you!";
+		private string victoryMessage1 = "You freed the\n@A\nwith @B\nenergy left!\nYou also got\n@C Ants!";
+		private string failureMessage2 = "The ants won!\nYou didn't free the\n@A!\nBut you got\n @C coins!";
+		private string failureMessage1 = "Aargh!\nOut of energy\nand didn't eat\nenough ants!\nTry again!";
 
 		private ScoreManager scoreManager;
 
@@ -28,41 +26,43 @@ namespace com.dogonahorse
 			string typeOfAnimal = scoreManager.ChapterAnimalName;
 			string resultsMessage;
 
-
 			//Players ran out of swipes - No coins, No nothin', Try again!
-			if (numberOfSwipes <= 0 && numberOfStars == 0) {
-				resultsMessage = ParseMessageString(failureMessage3,numberOfSwipes,numberOfCoins,typeOfAnimal);
-			}
-			else if (numberOfStars < 1) {
-				//Players lost
+			if (numberOfStars == 0 ) 
+			{
+				//Player lost.
 				resultsMessage = ParseMessageString(failureMessage1,numberOfSwipes,numberOfCoins,typeOfAnimal);
-				if (numberOfCoins > 0) {
-					resultsMessage += "\n" + ParseMessageString(failureMessage2,numberOfSwipes,numberOfCoins,typeOfAnimal); 
-				}
-			} else {
-				//Players won
+			}
+			else if (numberOfStars > 2) 
+			{
+				//Players got out with 0 or more energy but only 60% or less ants (2 stars).
+				resultsMessage = ParseMessageString(failureMessage2,numberOfSwipes,numberOfCoins,typeOfAnimal);
+			} else 
+			{
+				//Players got out with 90% or more ants (3 stars)and with 0 or more energy.
 				resultsMessage = ParseMessageString(victoryMessage1,numberOfSwipes,numberOfCoins,typeOfAnimal);
-				if (numberOfCoins > 0) {
-					resultsMessage += "\n" + ParseMessageString(victoryMessage2,numberOfSwipes,numberOfCoins,typeOfAnimal); 
-				}
 			}
 			Text messageText = GetComponentInChildren<Text>();
-		//	print (messageText);
+			//print (messageText);
 			messageText.text = resultsMessage;
 			SetStars(numberOfStars);
 		}
 
 		void SetStars (int numberOfStars) {
-	
+			
 			if (numberOfStars > 2) {
 				transform.Find("Star3").GetComponent<Image>().color = Color.red;
 				transform.Find("Star2").GetComponent<Image>().color = Color.red;
 				transform.Find("Star1").GetComponent<Image>().color = Color.red;
-			} else if (numberOfStars > 1){
+			} else if (numberOfStars > 1)
+			{
 				transform.Find("Star2").GetComponent<Image>().color = Color.red;
 				transform.Find("Star1").GetComponent<Image>().color = Color.red;
-			} else if (numberOfStars == 0){
+			} else if (numberOfStars > 0)
+			{
 				transform.Find("Star1").GetComponent<Image>().color = Color.red;
+			} else if (numberOfStars == 0)
+			{
+				//It will give 3 black stars
 			}
 		}
 
