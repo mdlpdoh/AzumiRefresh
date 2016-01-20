8a846b80b834c4bb3616154dfd1a7415d463ae21
Maria Mercedes Martinez <maria@dogonahorse.com>
2016-01-20 17:51:46 -0500

    improve the fail/victory messages

--- Assets/Resources/Scripts/UIScripts/LevelResultsModalWindow.cs
+++ Assets/Resources/Scripts/UIScripts/LevelResultsModalWindow.cs
@@ -4,58 +4,57 @@ using System.Collections;
 
 namespace com.dogonahorse
 {
+	public enum WinOrLoseCondition
+	{
+		// Win/Lose Conditions
+		Won,
+		LostNotEnufAnts,
+		LostNoEnergy,
+		LostNoTimeLeft
+
+	}
 
     public class LevelResultsModalWindow : ModalWindow
     {
         //victory messages
         private string victoryMessage1 = "You freed the @A\nwith @B energy left and\ngot @C Ants!";
         //failure messages
-        private string failureMessage1 = "Aargh!\nOut of energy and you didn't eat\nenough ants! Try again!"; // You lost, did not eat enuf ants and ran out of swipes.
-        private string failureMessage2 = "Oh No!\nYou ran out of energy and\ndid not free the@A!\nTry again!"; // You lost, got the min amt of ants but you ran out of swipes. 
-        private string failureMessage3 = "The ants got you!\nYou didn't eat enough of them! Try again!"; // You lost, did not eat min amt of ants but you still had swipes left.
+		private string failureMessage1 = "Aargh!\nYou didn't get out in time!\n The ants got you! Try again!";
+		private string failureMessage2 = "Oh No!\nYou ran out of energy and\ndid not free the @A! Try again!";
+		private string failureMessage3 = "The ants got you!\nYou didn't eat enough of them! Try again!";
 
         private ScoreManager scoreManager;
         public Material failPandaMat;
 
         override public void InitWindow()
         {
-
-            scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
-           //. int correctWinOrLoseMessage = scoreManager.WinAndLoseMessageInfo;
+			scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
+			// int correctWinOrLoseMessage = scoreManager.WinAndLoseMessageInfo;
             int numberOfSwipes = scoreManager.numberOfBounces;
             int numberOfCoins = scoreManager.CoinsEarned;
             int totalScore = scoreManager.TotalScore;
             string typeOfAnimal = scoreManager.ChapterAnimalName;
             string resultsMessage;
 
-            if (WinAndLoseMessageInfo == 0)
-            {
-                // 0 stars. You lost, did not eat min amt of ants but you still had swipes left.
-                resultsMessage = ParseMessageString(failureMessage3, numberOfSwipes, numberOfCoins, totalScore, typeOfAnimal);
-            }
-            else if (WinAndLoseMessageInfo == 5)
-            {
-                // 0 stars. You lost, did not eat enuf ants and ran out of swipes.
-                resultsMessage = ParseMessageString(failureMessage1, numberOfSwipes, numberOfCoins, totalScore, typeOfAnimal);
-            }
-            else if (WinAndLoseMessageInfo == 4)
-            {
-                // 0 stars. You lost, got the min amt of ants but you ran out of swipes. 
-                resultsMessage = ParseMessageString(failureMessage2, numberOfSwipes, numberOfCoins, totalScore, typeOfAnimal);
-            }
+			// pairs the win or lose condition with a message
+			if (GetWinOrLoseCondition () == WinOrLoseCondition.Won) 
+			{
+				resultsMessage = ParseMessageString (victoryMessage1, numberOfSwipes, numberOfCoins, totalScore, typeOfAnimal);	
 
-            else if (WinAndLoseMessageInfo > 0)
-            {
-                // 1 or more stars. You got min amt of ants in amt of swipes given.
-                resultsMessage = ParseMessageString(victoryMessage1, numberOfSwipes, numberOfCoins, totalScore, typeOfAnimal);
-            }
-            else
-            {
-                //Players got out with 0 or more energy and the min amt of ants or more - 1 or more stars.
-                resultsMessage = ParseMessageString(victoryMessage1, numberOfSwipes, numberOfCoins, totalScore, typeOfAnimal);
-            }
-
-
+			} 
+			else if (GetWinOrLoseCondition () == WinOrLoseCondition.LostNoTimeLeft) 
+			{
+				resultsMessage = ParseMessageString (failureMessage1, numberOfSwipes, numberOfCoins, totalScore, typeOfAnimal);
+			} 
+			else if (GetWinOrLoseCondition () == WinOrLoseCondition.LostNotEnufAnts) 
+			{
+				resultsMessage = ParseMessageString (failureMessage3, numberOfSwipes, numberOfCoins, totalScore, typeOfAnimal);
+			} 
+			else 
+			{
+				resultsMessage = ParseMessageString (failureMessage2, numberOfSwipes, numberOfCoins, totalScore, typeOfAnimal);
+			}
+				
             //Sends the various text information about score and swipes to the modal 
             Transform[] childTransforms = GetComponentsInChildren<Transform>();
             for (int i = 0; i < childTransforms.Length; i++)
@@ -74,44 +73,30 @@ namespace com.dogonahorse
 
             }//end for loop
         }//end method
-        public int WinAndLoseMessageInfo
-        // This gives the LevelResultsModalWindow information so it can spit out correct win/lose message
-        // number of stars given based on percentage of ants(coins) cleared in level
-        {
-            // return reference to private instance 
-            get
-            {
+	
+
+		WinOrLoseCondition GetWinOrLoseCondition()
+		{
+			if (scoreManager.NumberOfStars > 0) 
+			{
+				return WinOrLoseCondition.Won;
+			} 
+			else if (scoreManager.RanOutOfTime == true) 
+			{
+				return WinOrLoseCondition.LostNoTimeLeft;
+			} 
+			else if (scoreManager.TotalScore == 0) 
+			{
+				return WinOrLoseCondition.LostNotEnufAnts;
+			}
+			else 
+			{
+				return WinOrLoseCondition.LostNoTimeLeft;
+			}
+		}
 
 
-                if (scoreManager.NumberOfStars == 3)
-                {
-                    return 3;
-                }
-                else if (scoreManager.NumberOfStars == 2)
-                {
-                    return 2;
-                }
-                else if (scoreManager.NumberOfStars == 1)
-                {
-                    return 1;
-                }
-                else if (scoreManager.NumberOfStars == 0 && scoreManager.ExitedDoorSafely == true)
-                {
-                    return 0;
-                }
-                else if (scoreManager.NumberOfStars == 0 && scoreManager.ExitedDoorSafely == false )
-                {
-
-                    return 5;
-                }
-                else
-                {
-                    return 0;
-                }
-            }
-        }
-
-        void SetStars(int correctWinOrLoseMessage)
+		void SetStars(int theNumberOfStars)
         {
 
             if (scoreManager.NumberOfStars == 0)
