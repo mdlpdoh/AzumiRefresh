534291be8148268786dd9ea85ba36ecd0a797449
Maria Mercedes Martinez <maria@dogonahorse.com>
2016-01-26 16:43:12 -0500

    Create a coin counter script to put in game when we need to count the coins in each level. Add correct coins in levels to the level data json

---
+++ Assets/CoinCounter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinCounter : MonoBehaviour {
+
+private GameObject [] wallet;
+private int coinAmt;
+	// Use this for initialization
+	void Start () {
+        wallet = GameObject.FindGameObjectsWithTag("ant");
+        print("The amount of ants in this level = " + wallet.Length);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
